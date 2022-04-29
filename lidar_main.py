8f11992a68e5f0727edb5bff5058485122a0c0fe
ThienNien <tncupi@gmail.com>
2022-04-29 13:17:13 +0700

    add_main_thread

--- lidar_main.py
+++ lidar_main.py
@@ -1,6 +1,11 @@
 from Lidar_cluster import cluster_Lidar
-lidar = cluster_Lidar("COM8",[720,960],10,0.03)
+from sender import send_sig
+import config as cf
+import threading
+lidar = cluster_Lidar("/dev/ttyUSB0",[720,960],10,0.03)
 if __name__ == "__main__":
-    lidar.get_data()
-    
+    thr1 = threading.Thread(target=lidar.get_data)
+    thr1.start()
+    thr2 = threading.Thread(target= send_sig, args=(cf.turn,"192.168.1.2"))
+    thr2.start()
     
